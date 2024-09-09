fff9febf15e4799a9c89602539793d818f73fcac
MOHAMMED ADEL <tabwbwt@gmail.com>
2024-09-09 08:31:16 +0200

    Update زخرفه.js

--- plugins/زخرفه.js
+++ plugins/زخرفه.js
@@ -1,43 +1,41 @@
-import fetch from 'node-fetch';
-
-let handler = async (m, { conn, text }) => {
-  // Split the text into words
-  let words = text.split(' ');
-
-  // The first word should be the key, the rest is the text to stylize
-  let key = words[0];
-  let textToStyle = words.slice(1).join(' ');
-
-  // If no key and text provided, show all styles of a default text
-  if (words.length === 0 || !key || !textToStyle) {
-    let defaultText = '𝑧ₑ𝑧ₒ_𝑏ₒ𝑡';
-    let styledTexts = await Promise.all([...Array(34).keys()].map(i => stylizeText(defaultText, i + 1)));
-    conn.reply(m.chat, styledTexts.join`\n📍`, m);
-    return;
-  }
-
-  // Check if the key is a number between 1 and 34
-  if (!Number.isInteger(+key) || +key < 1 || +key > 34) {
-    throw 'مفتاح غير صالح. يرجى توفير رقم بين 1 و 34.';
-  }
-
-  // Get the styled text
-  let styledText = await stylizeText(textToStyle, key);
-
-  conn.reply(m.chat, styledText, m);
+function handler(m, { text }) {
+  let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
+  m.reply(teks.replace(/[ا-ي]/gi, v => {
+      return { 
+         'ا': 'آ',
+        'ب': 'بـًٌٍّ̨̥̬̩ـ',
+        'ت': 'تـًٌٍّ̨̥̬̩ـ',
+        'ث': 'ثـًٌٍّ̨̥̬̩ـ',
+        'ج': 'جـًٌٍّ̨̥̬̩ـ',
+        'ح': 'حـًٌٍّ̨̥̬̩ـ',
+        'خ': 'خـًٌٍّ̨̥̬̩ـ',
+        'د': 'دٍ',
+        'ذ': 'ذِ',
+        'ر': 'رٍ',
+        'ز': 'زٍ',
+        'س': 'سـًٌٍّ̨̥̬̩ـ',
+        'ش': 'شـًٌٍّ̨̥̬̩ـ',
+        'ص': 'صـًٌٍّ̨̥̬̩ـ',
+        'ض': 'ضـًٌٍّ̨̥̬̩ـ',
+        'ط': 'طـًٌٍّ̨̥̬̩ـ',
+        'ظ': 'ظـًٌٍّ̨̥̬̩ـ',
+        'ع': 'عـًٌٍّ̨̥̬̩ـ',
+        'غ': 'غـًٌٍّ̨̥̬̩ـ',
+        'ف': 'فـًٌٍّ̨̥̬̩ـ',
+        'ق': 'قـًٌٍّ̨̥̬̩ـ',
+        'ك': 'كـًٌٍّ̨̥̬̩ـ',
+        'ل': 'لَ',
+        'م': 'مـًٌٍّ̨̥̬̩ـ',
+        'ن': 'نـًٌٍّ̨̥̬̩ـ',
+        'ه': 'ه',
+        'ة': 'هّ',
+        'و': 'وٌ',
+        'ي': 'يـًٌٍّ̨̥̬̩ـ'
+      }[v.toLowerCase()] || v
+  }))
 }
+handler.help = ['H O D A']
+handler.tags = ['H O D A']
+handler.command =  /^(خط-عربي|زخرفه)$/i
 
-handler.help = ['style'].map(v => v + ' <key> <text>');
-handler.tags = ['tools'];
-handler.command = /^(زخرفه|زغرفه)$/i;
-handler.exp = 0;
-
-export default handler;
-
-async function stylizeText(text, key) {
-  let res = await fetch(`https://inrl-web-fkns.onrender.com/api/fancy?text=${encodeURIComponent(text)}&key=${key}`);
-  let data = await res.json();
-
-  // Use 'result' field for the styled text.
-  return `*Key ${key}*\n${data.result}`;
-}
+export default handler
