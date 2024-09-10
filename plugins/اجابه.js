de21374bbcc61d6e2c2b1d37f8f598ccc90a6a1d
MOHAMMED ADEL <tabwbwt@gmail.com>
2024-09-10 02:00:29 +0200

    Update اجابه.js

--- plugins/اجابه.js
+++ plugins/اجابه.js
@@ -1,14 +1,25 @@
-//كود اجابه
-
-let handler = async (m, { conn }) => {
-  conn.tebakbendera = conn.tebakbendera ? conn.tebakbendera : {}
-  let id = m.chat
-  if (!(id in conn.tebakbendera)) throw false
-  let json = conn.tebakbendera[id][1]
-  conn.reply(m.chat, '```' + json.name.replace(/[AIUEOaiueo]/ig, '_') + '```', m)
+import similarity from 'similarity'
+const threshold = 0.72
+let handler = m => m
+handler.before = async function (m) {
+    let id = m.chat
+    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/^ⷮ/i.test(m.quoted.text)) return !0
+    this.tekateki = this.tekateki ? this.tekateki : {}
+    if (!(id in this.tekateki)) return m.reply('Ese acertijo ya ha terminado!')
+    if (m.quoted.id == this.tekateki[id][0].id) {
+        let json = JSON.parse(JSON.stringify(this.tekateki[id][1]))
+        // m.reply(JSON.stringify(json, null, '\t'))
+        if (m.text.toLowerCase() == json.response.toLowerCase().trim()) {
+            global.db.data.users[m.sender].exp += this.tekateki[id][2]
+            m.reply(`*اجـابـة صـحـيـحـة✅ ❯*\n\n*الـجـائـزة💰↞ ${this.tekateki[id][2]} نقطة*`)
+            clearTimeout(this.tekateki[id][3])
+            delete this.tekateki[id]
+        } else if (similarity(m.text.toLowerCase(), json.response.toLowerCase().trim()) >= threshold) m.reply(`اقتربت من الاجابه!`)
+        else m.reply('اجـابـة خـاطـئـة❌ ❯')
+    }
+    return !0
 }
-handler.command = /اجابه$/i
 
-
+handler.exp = 0
 
 export default handler
