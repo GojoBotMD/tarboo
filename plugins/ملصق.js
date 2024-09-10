2d54b4a3ab878092d4c8a35223abcecca914b11d
MOHAMMED ADEL <tabwbwt@gmail.com>
2024-09-10 04:01:44 +0200

    Update ملصق.js

--- plugins/ملصق.js
+++ plugins/ملصق.js
@@ -1,52 +1,51 @@
-import { Sticker } from 'wa-sticker-formatter';
+import { sticker } from '../lib/sticker.js';
+import uploadFile from '../lib/uploadFile.js';
+import uploadImage from '../lib/uploadImage.js';
+import { webp2mp4 } from '../lib/webp2mp4.js';
 
-let handler = async (m, { conn }) => {
-  let img1 = 'https://i.pinimg.com/474x/81/24/d7/8124d7d7b4f8a31ebd69357077f0395c.jpg';
-  let img2 = 'https://i.pinimg.com/474x/48/7c/6a/487c6ac5b53248a5c5a2517e4c8438f5.jpg';
-  let img3 = 'https://i.pinimg.com/236x/02/6a/d8/026ad862da1132b0a9b4d5bb1f313276.jpg';
-  let img4 = 'https://i.pinimg.com/474x/e9/2f/fa/e92ffab0b31aab842f522af57eb61e61.jpg';
-  let img5 = 'https://i.pinimg.com/236x/cf/d2/23/cfd223d2294c355cb8f1c34ccc0bfa09.jpg';
-  let img6 = 'https://i.pinimg.com/474x/38/f0/88/38f088eb5daca8eb5e4ff078f875da20.jpg';
-  let img7 = 'https://i.pinimg.com/236x/f1/c3/bb/f1c3bbbefe1c3877d6d3f03be1f23c16.jpg';
-  let img8 = 'https://i.pinimg.com/474x/d0/16/46/d016461f195410021b191dd0f72ab37f.jpg';
-  let img9 = 'https://i.pinimg.com/236x/7e/32/3e/7e323ea0d39f5e9982a0cc1b3e3bc2cb.jpg';
-  let img10 = 'https://i.pinimg.com/474x/f4/3f/a0/f43fa059fd79bb90ae0bc78854b66fe8.jpg';
-  let randomNumber = Math.floor(Math.random() * 9);
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+let stiker = false
+try {
+let q = m.quoted ? m.quoted : m
+let mime = (q.msg || q).mimetype || q.mediaType || ''
+if (/webp|image|video/g.test(mime)) {
+if (/video/g.test(mime)) if ((q.msg || q).seconds > 8) return m.reply('*لا يمكن أن يزيد الفيديو عن 7 ثوانٍ*')
+let img = await q.download?.()
 
-  let sticker;
-  if (randomNumber === 0) {
-    sticker = await createSticker(false, img1, 'تــمــد يــدك اكــســرهــا˼😊˹\ 𝚂𝚑𝚊𝚛𝚔𝚢᯽𝚂𝚊𝚖𝚊あ', '', 20);
-  } else if (randomNumber === 1) {
-    sticker = await createSticker(false, img2, 'تــمــد يــدك اكــســرهــا˼😊˹\ 𝑮𝒐𝒌𝒖᯽𝚂𝚊𝚖𝚊あ', '', 20);
-  } else if (randomNumber === 2) {
-    sticker = await createSticker(false, img3, 'تــمــد يــدك اكــســرهــا˼😊˹\ 𝑮𝒐𝒌𝒖᯽𝚂𝚊𝚖𝚊あ', '', 20);
-  } else if (randomNumber === 3) {
-    sticker = await createSticker(false, img4, 'تــمــد يــدك اكــســرهــا˼😊˹\ 𝑮𝒐𝒌𝒖᯽𝚂𝚊𝚖𝚊あ', '', 20);
-  } else if (randomNumber === 4) {
-    sticker = await createSticker(false, img5, 'تــمــد يــدك اكــســرهــا˼😊˹\ 𝑮𝒐𝒌𝒖᯽𝚂𝚊𝚖𝚊あ', '', 20);
-  } else if (randomNumber === 5) {
-    sticker = await createSticker(false, img6, 'تــمــد يــدك اكــســرهــا˼😊˹\ 𝑮𝒐𝒌𝒖᯽𝚂𝚊𝚖𝚊あ', '', 20);
-  } else if (randomNumber === 6) {
-    sticker = await createSticker(false, img7, 'تــمــد يــدك اكــســرهــا˼😊˹\ 𝑮𝒐𝒌𝒖᯽𝚂𝚊𝚖𝚊あ', '', 20);
-  } else if (randomNumber === 7) {
-    sticker = await createSticker(false, img8, 'تــمــد يــدك اكــســرهــا˼😊˹\ 𝑮𝒐𝒌𝒖᯽𝚂𝚊𝚖𝚊あ', '', 20);
-  } else if (randomNumber === 8) {
-    sticker = await createSticker(false, img9, 'تــمــد يــدك اكــســرهــا˼😊˹\ 𝑮𝒐𝒌𝒖᯽𝚂𝚊𝚖𝚊あ', '', 20);
-  } else if (randomNumber === 9) {
-    sticker = await createSticker(false, img10, 'تــمــد يــدك اكــســرهــا˼😊˹\ 𝑮𝒐𝒌𝒖᯽𝚂𝚊𝚖𝚊あ', '', 20);
-  } else if (randomNumber === 10) {
+if (!img) throw `*أجب على مقطع فيديو أو صورة أو أدخل رابط إنهاء صورة. ‏jpg والتي سيتم تحويلها إلى ملصق ، يجب عليك الإجابة أو استخدام الأمر ${usedPrefix + command}*`
 
-  }
+let out
+try {
+stiker = await sticker(img, false, global.packname, global.author)
+} catch (e) {
+console.error(e)
+} finally {
+if (!stiker) {
+if (/webp/g.test(mime)) out = await webp2mp4(img)
+else if (/image/g.test(mime)) out = await uploadImage(img)
+else if (/video/g.test(mime)) out = await uploadFile(img)
+if (typeof out !== 'string') out = await uploadImage(img)
+stiker = await sticker(false, out, global.packname, global.author)
+}}
+} else if (args[0]) {
+if (isUrl(args[0])) stiker = await sticker(false, args[0], global.packname, global.author)
 
-  m.reply(sticker);
-};
+else return m.reply('*عنوان URL / الرابط غير صالح ، يجب أن يكون إنهاء الرابط / URL / الرابط هو ‏jpg ، على سبيل المثال .ملصق https://telegra.ph/file/5f6d20951b3930d99b306.jpg*')
 
-handler.customPrefix = /^(وينك|كيفك|شتسوي|شتسوي؟|ايش تسوي؟|كيفك؟|غوكو شتسوي|غوكو شتسوي؟|ايش تسوي)$/i;
-handler.command = new RegExp;
-
-export default handler;
-
-async function createSticker(img, url, packName, authorName, quality) {
-  let stickerMetadata = { type: 'full', pack: packName, packname: authorName, quality };
-  return (new Sticker(img ? img : url, stickerMetadata)).toBuffer();
 }
+} catch (e) {
+console.error(e)
+if (!stiker) stiker = e
+} finally {
+if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
+
+else throw '*خطأ ، يرجى المحاولة مرة أخرى. لا تنسي الرد على مقطع فيديو أو صورة أو إدراج رابط إنهاء الصورة.jpg‏ الذي سيتم تحويله الي ملصق*'
+
+}}
+handler.help = ['stiker (caption|reply media)', 'stiker <url>', 'stikergif (caption|reply media)', 'stikergif <url>']
+handler.tags = ['sticker']
+handler.command = /^ستيك|ملصق?$/i
+export default handler
+
+const isUrl = (text) => {
+return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png)/, 'gi'))}
